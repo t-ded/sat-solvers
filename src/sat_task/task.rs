50ee0f2b0bf8181ed04382ffb0d75440f2efc9b4
Tomáš Děd <tomas-ded@seznam.cz>
2024-10-08 08:43:24 +0200

    Rework task to be able to mask clauses and literals

--- src/sat_task/task.rs
+++ src/sat_task/task.rs
@@ -1,33 +1,45 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
 pub struct Clause {
-    pub literals: HashSet<isize>,
+    pub literals: HashMap<isize, bool>,
+    pub n_literals: usize,
 }
 
 impl Clause {
     pub fn empty() -> Clause {
-        Clause { literals: HashSet::new() }
+        Clause { literals: HashMap::new(), n_literals: 0 }
     }
 
-    pub fn from_set(literals: HashSet<isize>) -> Clause {
-        Clause { literals }
+    pub fn from_set(literal_set: HashSet<isize>) -> Clause {
+        Clause {
+            n_literals: literal_set.len(),
+            literals: literal_set.into_iter().map(|lit| (lit, true)).collect(),
+        }
     }
 
-    pub fn from_iter<I>(iter: I) -> Clause
+    pub fn from_literal_iter<I>(iter: I) -> Clause
     where
         I: IntoIterator<Item = isize>,
     {
-        Clause { literals: iter.into_iter().collect() }
+        let mut clause = Clause {
+            literals: iter.into_iter().map(|lit| (lit, true)).collect(),
+            n_literals: 0,
+        };
+        clause.n_literals = clause.literals.len();
+        clause
     }
 
     pub fn is_satisfied(&self, assignment: &Vec<Option<bool>>) -> bool {
-        for literal in self.literals.iter() {
-            let literal_idx = (literal.abs() - 1) as usize;
-            if assignment[literal_idx].is_none() { return false }
-            else {
-                if *literal > 0 && assignment[literal_idx].unwrap() == false { return false }
-                if *literal < 0 && assignment[literal_idx].unwrap() == true { return false }
+        if self.n_literals == 0 { return false }
+        for (literal, literal_mask) in self.literals.iter() {
+            if *literal_mask {
+                let literal_idx = (literal.abs() - 1) as usize;
+                if assignment[literal_idx].is_none() { return false }
+                else {
+                    if *literal > 0 && assignment[literal_idx].unwrap() == false { return false }
+                    if *literal < 0 && assignment[literal_idx].unwrap() == true { return false }
+                }
             }
         }
         true
@@ -37,11 +49,18 @@ impl Clause {
 #[derive(Debug)]
 pub struct Task {
     pub n_variables: usize,
+    pub n_clauses: usize,
     pub clauses: Vec<Clause>,
+    mask: Vec<bool>,
 }
 
 impl Task {
     pub fn empty(n_variables: usize, n_clauses: usize) -> Task {
-        Task { n_variables, clauses: Vec::with_capacity(n_clauses) }
+        Task { n_variables, n_clauses, clauses: Vec::with_capacity(n_clauses), mask: vec![true; n_clauses] }
+    }
+
+    pub fn remove_nth_clause(&mut self, n: usize) {
+        self.mask[n] = false;
+        self.n_clauses -= 1;
     }
 }
