16fe603c37bf747ca0a4d2111608e5d00ba3067a
Tomáš Děd <tomas-ded@seznam.cz>
2024-10-08 08:43:43 +0200

    Add string and file parse for basic correct input

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,14 +1,68 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::path::Path;
 use crate::sat_task::task::Task;
+use crate::sat_task::task::Clause;
 
 
 pub struct Parser {}
 
 impl Parser {
-    fn parse_file(&self, path: &str) -> Task {
-        Task::empty()
+
+    fn parse_file<P: AsRef<Path>>(&self, file_path: P) -> io::Result<Task> {
+        let mut file = File::open(file_path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        let task = self.parse_str(&contents);
+        Ok(task)
     }
 
     fn parse_str(&self, input: &str) -> Task {
-        Task::empty()
+        let mut input_lines = input.lines();
+        let mut header = input_lines.next().unwrap().split_whitespace();
+        let n_variables = header.nth(2).unwrap().parse().unwrap();
+        let n_clauses = header.next().unwrap().parse().unwrap();
+
+        let mut task = Task::empty(n_variables, n_clauses);
+        for line in input_lines {
+            task.clauses.push(
+                Clause::from_literal_iter(
+                    line.rsplit_once('0')
+                        .unwrap()
+                        .0
+                        .split_whitespace()
+                        .map(|x| x.parse::<isize>().unwrap())
+                )
+            );
+        }
+        task
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::iter::zip;
+    use super::*;
+
+    #[test]
+    fn test_parse_basic_str() {
+        let parser = Parser {};
+        let task = parser.parse_str(
+            "p cnf 5 3
+            1 -5 4 0
+            -1 5 3 4 0
+            -3 -4 0"
+        );
+        let expected = [
+            HashSet::from_iter([1, -5, 4]),
+            HashSet::from_iter([-1, 5, 3, 4]),
+            HashSet::from_iter([-3, -4])
+        ];
+        for (clause, expected_literals) in zip(&task.clauses, expected.iter()) {
+            assert_eq!(&clause.literals.keys().cloned().collect::<HashSet<isize>>(), expected_literals)
+        }
     }
 }
