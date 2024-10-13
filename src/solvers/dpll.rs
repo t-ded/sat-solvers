fed15bf8cd8ed0c1af3b4e6f54174a51362830a3
Tomáš Děd <tomas-ded@seznam.cz>
2024-10-13 10:31:44 +0200

    Add pure literal removal to dpll but turn both preprocessing steps off until indexing fixed

--- src/solvers/dpll.rs
+++ src/solvers/dpll.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use crate::sat_task::task::{Clause, Task};
 use crate::solvers::solution_outcomes::TaskResult;
 use crate::solvers::solution_outcomes::TaskResult::UNSAT;
@@ -5,35 +6,45 @@ use std::ops::Neg;
 
 pub struct DPLLSolver {
     cache_removed_clauses: Vec<Clause>,
-    cache_clauses_with_removed_literal: Vec<usize>,
+    cache_clauses_with_removed_literal: Vec<(usize, isize)>,
+    cache_pure_literals: Vec<usize>,
 }
 
 impl DPLLSolver {
     pub fn new() -> Self {
-        DPLLSolver { cache_removed_clauses: Vec::new(), cache_clauses_with_removed_literal: Vec::new() }
+        DPLLSolver { cache_removed_clauses: Vec::new(), cache_clauses_with_removed_literal: Vec::new(), cache_pure_literals: Vec::new() }
     }
 
     pub fn solve(&mut self, input: &mut Task, chosen_lit: usize) -> TaskResult {
         if input.is_solved() {
             self.return_sat_assignment(&input)
         } else if input.is_solvable() && chosen_lit <= input.n_variables {
+            // self.unit_propagation(input);
+            // if input.contains_empty_clause() {
+            //     self.revert_unit_propagation(input);
+            //     return UNSAT("TODO")
+            // }
+            // self.pure_literal_removal(input);
+            // if input.n_clauses == 0 {
+            //     self.return_sat_assignment(input);
+            // }
+
             input.assign_literal(chosen_lit, Some(true));
             input.add_clause(Clause::from_literal_iter(std::iter::once(chosen_lit as isize)));
-            self.unit_propagation(input);
             if let TaskResult::SAT(assignment) = self.solve(input, chosen_lit + 1) {
                 return TaskResult::SAT(assignment)
             }
-            self.revert_unit_propagation(input, chosen_lit as isize);
+            input.remove_nth_clause(input.n_clauses - 1);
 
-            let neg_lit = -(chosen_lit as isize);
             input.assign_literal(chosen_lit, Some(false));
-            input.add_clause(Clause::from_literal_iter(std::iter::once(neg_lit)));
-            self.unit_propagation(input);
+            input.add_clause(Clause::from_literal_iter(std::iter::once(-(chosen_lit as isize))));
             if let TaskResult::SAT(assignment) = self.solve(input, chosen_lit + 1) {
                 return TaskResult::SAT(assignment)
             }
-            self.revert_unit_propagation(input, neg_lit);
+            input.remove_nth_clause(input.n_clauses - 1);
 
+            // self.revert_unit_propagation(input);
+            // self.revert_pure_literal_removal(input);
             input.assign_literal(chosen_lit, None);
             UNSAT("TODO")
         } else {
@@ -53,7 +64,7 @@ impl DPLLSolver {
                 } else {
                     if input.clauses[i].literals.contains(&unit_literal.neg()) {
                         input.clauses[i].remove_literal(unit_literal.neg());
-                        self.cache_clauses_with_removed_literal.push(i);
+                        self.cache_clauses_with_removed_literal.push((i, unit_literal.neg()));
                     }
                     i += 1;
                 }
@@ -61,7 +72,7 @@ impl DPLLSolver {
         }
     }
 
-    fn find_unit_literal(&self, input: &'_ Task) -> isize {
+    fn find_unit_literal(&self, input: &Task) -> isize {
         for clause in input.clauses.iter() {
             if clause.n_literals == 1 {
                 return *clause.literals.iter().next().unwrap()
@@ -70,47 +81,65 @@ impl DPLLSolver {
         0
     }
 
-    fn revert_unit_propagation(&mut self, input: &mut Task, chosen_lit: isize) {
-        while let Some(clause_idx) = self.cache_clauses_with_removed_literal.pop() {
-            input.clauses[clause_idx].add_literal(chosen_lit);
+    fn revert_unit_propagation(&mut self, input: &mut Task) {
+        while let Some((clause_idx, removed_lit)) = self.cache_clauses_with_removed_literal.pop() {
+            input.clauses[clause_idx].add_literal(removed_lit);
         }
         while let Some(clause) = self.cache_removed_clauses.pop() {
             input.add_clause(clause);
         }
     }
 
-        // self.unit_propagation(input);
-        // for clause in &input.clauses {
-        //     if clause.n_literals == 0 { return TaskResult::UNSAT("TODO") }
-        // }
-        // self.propagate_pure_literals(input);
-        // if input.n_clauses == 0 {
-        //     return TaskResult::SAT(
-        //         input.assignment
-        //             .clone()
-        //             .into_iter()
-        //             .map(|(k, v)| (k, v.expect("SAT assignment should have all literals assigned")))
-        //             .collect()
-        //     )
-        // }
+    pub fn pure_literal_removal(&mut self, input: &mut Task) {
+        loop {
+            let pure_literal = self.find_pure_literal(input);
+            if pure_literal == 0 { break }
+            if pure_literal > 0 { input.assignment.insert(pure_literal as usize, Some(true)); }
+            else { input.assignment.insert(pure_literal.unsigned_abs(), Some(false)); }
+            self.cache_pure_literals.push(pure_literal.unsigned_abs());
+            // let mut i = 0;
+            // while i < input.n_clauses {
+            //     if input.clauses[i].literals.contains(&pure_literal) {
+            //         self.cache_removed_clauses.push(input.remove_nth_clause(i));
+            //     } else { i += 1 }
+            // }
+        }
+    }
 
-        // if chosen_lit.unsigned_abs() <= input.n_variables {
-        //     let next_chosen_lit = if chosen_lit > 0 {
-        //         input.assign_literal(chosen_lit.unsigned_abs(), Some(true));
-        //         -chosen_lit
-        //     } else {
-        //         input.assign_literal(chosen_lit.unsigned_abs(), Some(false));
-        //         -chosen_lit + 1
-        //     };
-        //     // input.add_clause(Clause::from_set(HashSet::from([chosen_lit])));
-        //     let result = self.solve(input, next_chosen_lit);
-        //     // input.remove_nth_clause(input.n_clauses);
-        //     input.assign_literal(chosen_lit.unsigned_abs(), None);
-        //     if let TaskResult::SAT(_) = result { return result }
-        // } else {
-        //     return if input.is_solved() { self.return_sat_assignment(input) } else { TaskResult::UNSAT("TODO") }
+    fn find_pure_literal(&self, input: &Task) -> isize {
+        let mut literal_sign_found: HashMap<isize, Option<isize>> = HashMap::from_iter(
+            input.assignment
+                .iter()
+                .filter_map(
+                    |(&key, &value)| {
+                        if value.is_none() {Some((key as isize, None))} else {None}
+                    }
+                )
+        );
+        for (literal, sign_found) in literal_sign_found.iter_mut() {
+            for clause in input.clauses.iter() {
+                if clause.literals.contains(literal) {
+                    if sign_found.is_none() { *sign_found = Some(1); }
+                    else if *sign_found == Some(-1) { *sign_found = Some(0); }
+                } else if clause.literals.contains(&(-literal)) {
+                    if sign_found.is_none() { *sign_found = Some(-1); }
+                    else if *sign_found == Some(1) { *sign_found = Some(0); }
+                }
+            }
+            if sign_found.is_none() || *sign_found == Some(1) { return *literal}
+            else if *sign_found == Some(-1) { return -*literal}
+        }
+        0
+    }
+
+    fn revert_pure_literal_removal(&mut self, input: &mut Task) {
+        // while let Some(clause) = self.cache_removed_clauses.pop() {
+        //     input.add_clause(clause);
         // }
-        //
+        while let Some(literal) = self.cache_pure_literals.pop() {
+            input.assignment.insert(literal, None);
+        }
+    }
 
     fn return_sat_assignment(&self, input: &Task) -> TaskResult {
         TaskResult::SAT(
@@ -121,47 +150,6 @@ impl DPLLSolver {
                 .collect()
         )
     }
-    //
-    //
-    // pub fn propagate_pure_literals(&self, mut input: &mut Task) {
-    //     loop {
-    //         let pure_literal = self.find_pure_literal(input);
-    //         if pure_literal == 0 { break }
-    //         for i in 0..input.n_clauses {
-    //             if let Some(&true) = input.clauses[i].literals.get(&pure_literal) {
-    //                 input.mask_nth_clause(i);
-    //             }
-    //         }
-    //     }
-    // }
-    //
-    // fn find_pure_literal(&self, input: &Task) -> isize {
-    //     let mut assignment_vec: Vec<Option<i8>> = vec![None; input.n_variables];
-    //     for (i, clause) in input.clauses.iter().enumerate() {
-    //         if !input.is_masked(i) {
-    //             for (literal, mask) in clause.literals.iter() {
-    //                 let assignment_idx = (literal.abs() - 1) as usize;
-    //                 let assignment_value = assignment_vec[assignment_idx];
-    //                 if *mask && assignment_value != Some(0) {
-    //                     if *literal > 0 {
-    //                         if assignment_value == Some(-1) { assignment_vec[assignment_idx] = Some(0); }
-    //                         else if assignment_value == None { assignment_vec[assignment_idx] = Some(1); }
-    //                     } else if *literal < 0 {
-    //                         if assignment_value == Some(1) { assignment_vec[assignment_idx] = Some(0); }
-    //                         else if assignment_value == None { assignment_vec[assignment_idx] = Some(-1); }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     for idx in 0..input.n_variables {
-    //         if let Some(literal) = assignment_vec[idx] {
-    //             if literal > 0 { return (idx + 1) as isize }
-    //             else if literal < 0 { return -((idx + 1) as isize) }
-    //         }
-    //     }
-    //     0
-    // }
 }
 
 
